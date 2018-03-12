792445e3c16a53573cc21b34fb6c3fc02a048241
caramirezal <cramireza@ciencias.unam.mx>
2018-03-12 14:07:58 -0600

    Add Manhattan plot

--- scripts/alleles.R
+++ scripts/alleles.R
@@ -1,15 +1,23 @@
+library(dplyr)
+library(ggplot2)
 
 ## make bed files from ped genomic data
-makeBed <- 'plink1 --file PLINK --make-bed --out alleles'
-system(makeBed)
+#makeBed <- 'plink1 --file PLINK --make-bed --out alleles'
+#system(makeBed)
 ## execute association analysis
-association <- 'plink1 --bfile alleles --assoc --out alleles'
-system(association)
+#association <- 'plink1 --bfile alleles --assoc --out alleles'
+#system(association)
 
 alleles <- read.table("alleles.assoc",
                    header = TRUE,
                    stringsAsFactors = FALSE)
 
-alleles <- alleles[!is.na(alleles$CHISQ),]
-tail(alleles)
-plot(seq_along(alleles$SNP),alleles$CHISQ)
+
+select <- select(alleles,CHR,BP,CHISQ)
+alleles <- filter(alleles,!is.na(CHISQ))
+alleles <- filter(alleles, CHR %in% 1:5)
+
+g <- ggplot(alleles,aes(BP,CHISQ))
+g <- g + geom_point(aes(colour=factor(CHR)))
+g <- g + theme_classic()
+plot(g)
