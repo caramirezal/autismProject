c3761bca396ee5ce628f4a606b60e5607d8ee23f
caramirezal <cramireza@ciencias.unam.mx>
2018-08-04 16:56:17 -0500

    testing ML on genomic data

---
+++ scripts/lasso.R
@@ -0,0 +1,32 @@
+library(dplyr)
+library(caret)
+library(glmnet)
+
+extractAlleles <- "plink --file /media/carlos/SoMobileOTB/Arrays/PsychArray\ AT\ 5p/U15_118_Placa5/PLINK/PLINK_060117_0128/PLINK --chr 2 --from-kb 1000 --to-kb 1100  --recodeAD --out data/extractedSNPsPlink"
+
+## pre reading
+alleles <- readLines("~/data/extractedSNPsPlink.raw",n=2)
+alleles <- read.table("~/data/extractedSNPsPlink.raw",sep = " ",nrows = 2,header = TRUE)
+
+## loading data
+alleles <- read.table("~/data/extractedSNPsPlink.raw",header = TRUE)
+dim(alleles)
+head(alleles)
+
+## preprocessing 
+## dropping some unwanted & constant variables
+dropVars <- c("IID","FID","PAT","MAT","SEX","PHENOTYPE")
+alleles.p <- select(alleles,-dropVars) %>%
+                select(-nearZeroVar(alleles.p))
+alleles.p <- alleles.p[complete.cases(alleles.p),]
+dim(alleles.p)
+head(alleles.p)
+
+## somulation of an output values as
+## output = Allele + noise 
+output <- alleles.p$rs6717701_G + rnorm(nrow(alleles.p),sd=0.35)
+output <- (output - min(output))/(max(output)-min(output))
+
+## performing lasso
+lasso <- cv.glmnet(y = output,x = as.matrix(alleles.p),nfolds = 10)
+logReg <- glm(rs11675840_HET~.,data = alleles.p,family = binomial(link = "logit"))
